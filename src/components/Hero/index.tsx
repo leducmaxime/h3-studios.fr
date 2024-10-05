4fec61d5799e3651bc706fb164028f921f52b045
Maxime Leduc <maxime@aldebaran.care>
2024-10-05 17:14:47 +0200

    CI deploy to Github Pages

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -15,7 +15,7 @@ const Hero = () => {
                   H3-STUDIOS
                 </h1>
                 <p className="mb-12 text-base !leading-relaxed text-body-color dark:text-body-color-dark sm:text-lg md:text-xl">
-                  Studio d'enregistrement - Répétitions - Entrecôtes
+                  {"Studio d'enregistrement - Répétitions - Entrecôtes"}
                 </p>
                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
                   <Link
