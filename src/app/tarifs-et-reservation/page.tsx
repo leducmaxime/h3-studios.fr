0ac3dc1b04733c8bafde7315f926f2497c6ab034
Maxime Leduc <maxime@aldebaran.care>
2024-10-24 21:38:08 +0200

    responsive

--- src/app/tarifs-et-reservation/page.tsx
+++ src/app/tarifs-et-reservation/page.tsx
@@ -21,40 +21,33 @@ export default async function Home() {
   return (
     <div className="flex min-h-fit grow flex-col items-center gap-8 pb-8 pt-24">
       <ScrollUp />
-      <div className="font-blanka text-3xl md:text-5xl">
+      <div className="text-center font-blanka text-3xl md:text-5xl">
         TARIFS ET RESERVATION
       </div>
-      <div className="flex w-full max-w-[1048px] flex-col gap-8 border-8 border-primary bg-black p-8">
+      <div className="flex w-full max-w-[1048px] flex-col gap-8">
         <div className="flex flex-col gap-8 lg:flex-row">
-          {/* <div className="flex items-center lg:basis-3/5"> */}
-          {/*   <ExportedImage */}
-          {/*     src={scene1} */}
-          {/*     alt={"Marcel HAMON"} */}
-          {/*     sizes="" */}
-          {/*     className="min-h-full object-cover" */}
-          {/*     priority */}
-          {/*   /> */}
-          {/* </div> */}
           <div className="flex w-full flex-col items-center gap-8 text-center">
-            <div className="flex w-full flex-col gap-8 bg-primary p-4 text-center text-black">
+            <div className="flex flex-col gap-8 bg-primary px-8 py-4 text-center text-black">
               <div className="w-full text-2xl font-bold underline decoration-4 underline-offset-8">
                 RÉSERVATION
               </div>
-              <div>
+              <div className="flex flex-col gap-2">
                 <p className="whitespace-pre-line">
                   {`Pour plus d'informations ou pour effectuer une réservation,
                     n'hésitez pas à contacter notre équipe par e-mail à `}
                 </p>
                 <a
-                  className="whitespace-nowrap font-semibold"
-                  href="mailto:contact@h3-studios.fr"
+                  className="whitespace-nowrap text-xl font-semibold"
+                  href="mailto:contact@h3-studios.fr?subject=Réservation+d%27un+studio"
+                  target="_blank"
                 >
                   contact@h3-studios.fr
                 </a>
                 <p>{"ou à joindre Marcel par téléphone au "}</p>
                 <a
-                  className="whitespace-nowrap font-semibold"
+                  className="whitespace-nowrap text-xl font-semibold"
                   href="tel:0613440875"
+                  target="_blank"
                 >
                   06 13 44 08 75
                 </a>
@@ -63,112 +56,107 @@ export default async function Home() {
             <div className="text-center font-blanka text-2xl">
               {"LES TARIFS"}
             </div>
-            <Table
-              theme={customTheme}
-              striped
-              className="border-8 border-primary text-center"
-            >
-              <TableHead>
-                <TableHeadCell></TableHeadCell>
-                <TableHeadCell>Studio La Scène</TableHeadCell>
-                <TableHeadCell>Studio Le Podium</TableHeadCell>
-              </TableHead>
-              <TableBody className="divide-y">
-                <TableRow>
-                  <TableCell>
-                    <div className="flex flex-col gap-2">
-                      <b>Groupes (3 ou +)</b>
-                      <span></span>
-                      <span>Avant 18h</span>
-                      <span> Après 18h / Week-end / Jours fériés</span>
-                    </div>
-                  </TableCell>
-                  <TableCell className="align-bottom">
-                    <div className="flex flex-col gap-2">
-                      <span>18€ / Heure</span>
-                      <span>22€ / Heure</span>
-                    </div>
-                  </TableCell>
-                  <TableCell className="align-bottom">
-                    <div className="flex flex-col gap-2">
-                      <span>15€ / Heure</span>
-                      <span>18€ / Heure</span>
-                    </div>
-                  </TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>
-                    <div className="flex flex-col">
-                      <b>Solo / Duo</b>
-                    </div>
-                  </TableCell>
-                  <TableCell>8€ / Heure</TableCell>
-                  <TableCell>6€ / Heure</TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>
-                    <div className="flex flex-col">
-                      <b>Prise de son</b>
-                      <span>(nous contacter pour le mixage éventuel)</span>
-                    </div>
-                  </TableCell>
-                  <TableCell colSpan={2}>
-                    50€ / Heure
-                    <br />
-                    160€ / Demi-Journée
-                    <br />
-                    320€ / Journée
-                  </TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>
-                    <b>Locations</b>
-                  </TableCell>
-                  <TableCell className="text-right">
-                    Cymbale crash :
-                    <br />
-                    Micro supplémentaire (5ème ou +) :
-                    <br />
-                    Instruments :
-                  </TableCell>
-                  <TableCell className="text-left">
-                    1€ / Heure
-                    <br />
-                    1€ / Heure{" "}
-                    <span className="text-xs">(plafonné à 3€ / séance)</span>
-                    <br /> 2€ / Heure{" "}
-                    <span className="text-xs">(plafonné à 5€ / séance)</span>
-                  </TableCell>
-                </TableRow>
-              </TableBody>
-            </Table>
-            {/* <div className="w-full text-2xl font-bold text-primary underline decoration-4 underline-offset-8"> */}
-            {/*   LOCATION STUDIO */}
-            {/* </div> */}
-            {/* <p className="w-full whitespace-pre-line">{`Tarif groupe : de 15€ à 22€ par Heure */}
-            {/*   Tarif enseignants/Solo : de 6€ à 8€ par Heure`}</p> */}
-            {/* <div className="w-full text-2xl font-bold text-primary underline decoration-4 underline-offset-8"> */}
-            {/*   ENREGISTREMENT */}
-            {/* </div> */}
-            {/* <p className="w-full whitespace-pre-line">{`1h : 50€ */}
-            {/*   Demi journée : 170€ */}
-            {/*   Journée : 320€`}</p> */}
-            {/* <div className="w-full text-2xl font-bold text-primary underline decoration-4 underline-offset-8"> */}
-            {/*   COURS DE BATTERIE */}
-            {/* </div> */}
-            {/* <p className="w-full whitespace-pre-line">{`Nous contacter`}</p> */}
-            {/* <div className="w-full text-2xl font-bold text-primary underline decoration-4 underline-offset-8"> */}
-            {/*   ATELIERS */}
-            {/* </div> */}
-            {/* <p className="w-full whitespace-pre-line">{`Nous contacter`}</p> */}
-            {/* <div className="w-full text-2xl font-bold text-primary underline decoration-4 underline-offset-8"> */}
-            {/*   PRIVATISATION */}
-            {/* </div> */}
-            {/* <p className="w-full whitespace-pre-line">{`Nous contacter`}</p> */}
-            {/* <div className="w-full text-2xl font-bold text-primary underline decoration-4 underline-offset-8"> */}
-            {/*   FORMATEURS */}
-            {/* </div> */}
-            {/* <p className="w-full whitespace-pre-line">{`Nous contacter`}</p> */}
+            <div className="flex w-full flex-col gap-4">
+              <Table
+                theme={customTheme}
+                striped
+                className="w-full border-8 border-primary text-center"
+              >
+                <TableHead>
+                  <TableHeadCell></TableHeadCell>
+                  <TableHeadCell>
+                    Studio <span className="whitespace-nowrap">La Scène</span>
+                  </TableHeadCell>
+                  <TableHeadCell>
+                    Studio <span className="whitespace-nowrap">Le Podium</span>
+                  </TableHeadCell>
+                </TableHead>
+                <TableBody className="divide-y">
+                  <TableRow>
+                    <TableCell>
+                      <b>
+                        Groupes{" "}
+                        <span className="whitespace-nowrap">(3 ou +)</span>
+                      </b>
+                      <br />
+                      Avant 18h
+                    </TableCell>
+                    <TableCell>18€/Heure</TableCell>
+                    <TableCell>15€/Heure</TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell>
+                      <b>
+                        Groupes{" "}
+                        <span className="whitespace-nowrap">(3 ou +)</span>
+                      </b>
+                      <br />
+                      <span className="whitespace-nowrap">
+                        Après 18h /
+                      </span>{" "}
+                      <span className="whitespace-nowrap">Week-end /</span>{" "}
+                      <span className="whitespace-nowrap">Jours fériés</span>
+                    </TableCell>
+                    <TableCell>22€/Heure</TableCell>
+                    <TableCell>18€/Heure</TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell>
+                      <div className="flex flex-col">
+                        <b>Solo / Duo</b>
+                      </div>
+                    </TableCell>
+                    <TableCell>8€/Heure</TableCell>
+                    <TableCell>6€/Heure</TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+              <Table
+                theme={customTheme}
+                striped
+                className="w-full border-8 border-primary text-center"
+              >
+                <TableBody className="divide-y">
+                  <TableRow>
+                    <TableCell>
+                      <div className="flex flex-col">
+                        <b>Prise de son</b>
+                        <span>(nous contacter pour le mixage éventuel)</span>
+                      </div>
+                    </TableCell>
+                    <TableCell>
+                      50€/Heure
+                      <br />
+                      160€/Demi-Journée
+                      <br />
+                      320€/Journée
+                    </TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell>
+                      <b>Locations</b>
+                    </TableCell>
+                    <TableCell>
+                      <div className="flex flex-col gap-2">
+                        <span>Cymbale crash : 1€/Heure</span>
+                        <span>
+                          Micro supplémentaire (5ème ou +) : 1€/Heure{" "}
+                          <span className="text-xs">
+                            (plafonné à 3€/séance)
+                          </span>
+                        </span>
+                        <span>
+                          Instruments : 2€/Heure{" "}
+                          <span className="text-xs">
+                            (plafonné à 5€/séance)
+                          </span>
+                        </span>
+                      </div>
+                    </TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </div>
           </div>
         </div>
       </div>
--- src/app/theme.ts
+++ src/app/theme.ts
@@ -10,7 +10,7 @@ export const customTheme: CustomFlowbiteTheme["table"] = {
   body: {
     base: "group/body",
     cell: {
-      base: "px-6 py-4 group-first/body:group-first/row:first:rounded-tl-lg group-first/body:group-first/row:last:rounded-tr-lg group-last/body:group-last/row:first:rounded-bl-lg group-last/body:group-last/row:last:rounded-br-lg",
+      base: "px-1 py-4 sm:px-6 group-first/body:group-first/row:first:rounded-tl-lg group-first/body:group-first/row:last:rounded-tr-lg group-last/body:group-last/row:first:rounded-bl-lg group-last/body:group-last/row:last:rounded-br-lg",
     },
   },
   head: {
