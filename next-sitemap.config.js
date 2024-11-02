4bc9b76c49ea2c22a566b5c17ec5fe56886d859e
Maxime Leduc <maxime@aldebaran.care>
2024-11-02 18:41:13 +0100

    robot & sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.SITE_URL || "https://h3-studios.fr",
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Home from "./home/page";
+import Home from "./_home/page";
 
 export default function App() {
   return <Home />;
